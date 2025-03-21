7c89aee33515f17bc0fd16cbf505388f9c73537f
CastleOneKill <castle371996@gmail.com>
2025-03-21 11:24:09 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/light-onlydomains.txt
+https://malware-filter.gitlab.io/malware-filter/vn-badsite-filter-domains.txt
 node download_lists.js blocklist
